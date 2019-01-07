696b69d6b83c71a4ce506128da890314f7603764
Tim West <twestWTCN@users.noreply.github.com>
2019-01-07 16:22:36 +0000

    Create Figure5A_5B.m

---
+++ For_Paper/Figures/legacy/Figure5A_5B.m
@@ -0,0 +1,27 @@
+close all; clear all
+%% Simulation 5A and B (SNR and Asym SNR Sweep)
+% Effects of Signal Mixing
+fname = 'Figure4';
+N = 3; % # of nodes
+MO = 3;% model order
+C = repmat(repmat(0.5,N,N).*eye(N),1,1,MO);
+C(:,:,2) = -C(:,:,2);
+C(2,1,2) = 0.5;
+C(3,1,3) = 0.5;
+NCV     = eye(N).*0.3;
+
+% 5A - FC vs SNR
+% % mvarconsim_npdver_F2b(C,NCV,25)
+figure(1)
+% mvarconsim_npdver_F3b(C,NCV,25)
+% 
+
+% 5A - FC vs SNR Asymmetry
+figure(2)
+C = repmat(repmat(0.5,N,N).*eye(N),1,1,MO);
+C(:,:,2) = -C(:,:,2);
+C(2,1,2) = 0.35;
+C(1,2,2) = 0.35;
+mvarconsim_npdver_F3b_asysm(C,NCV,25)
+
+
